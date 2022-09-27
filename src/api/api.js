abee9a39d8f4d7c56ee8c4a7017e50afe078113f
Amanyadav2030 <Amanyadav919821@gmail.com>
2022-09-27 09:21:20 +0530

    done

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export function getAllProduct(){
-    return axios.get('https://safe-hollows-22804.herokuapp.com/bellavita').then((res)=>{
+    return axios.get(process.env.REACT_APP_FORM_API_KEY).then((res)=>{
         return res;
     })
 }
 export function getsingleData(id){
-    return axios.get(`https://safe-hollows-22804.herokuapp.com/bellavita/${id}`).then((res)=>{
+    return axios.get(`${process.env.REACT_APP_FORM_API_KEY}/${id}`).then((res)=>{
         return res;
     })
 }
